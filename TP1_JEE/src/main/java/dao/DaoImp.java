2a4509985cbb2884d2691c04e50b8dbc7635307c
Yassine Serroukhe Idrissi <yassine.serroukhe@gmail.com>
2023-03-22 22:34:50 +0000

    TP1

---
+++ TP1_JEE/src/main/java/dao/DaoImp.java
@@ -0,0 +1,12 @@
+package dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DaoImp implements IDao{
+    @Override
+    public double getData() {
+        System.out.println("From SQL DB");
+        return (7);
+    }
+}
---
+++ TP1_JEE/src/main/java/ma/yassine/serroukhe/idrissi/Main.java
@@ -0,0 +1,7 @@
+package ma.yassine.serroukhe.idrissi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
